b97a22c10103dd5bae09fbcaa995aa4d85325234
nadan.choi <nadan.choi@kakaocorp.com>
2018-09-19 18:57:01 +0900

    .

--- source/handWriting.py
+++ source/handWriting.py
@@ -13,3 +13,13 @@ W2 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
 L2 = tf.nn.relu(tf.matmul(L1, W2))
 
 W3 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
+model = tf.matmul(L2, W3)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
+optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
